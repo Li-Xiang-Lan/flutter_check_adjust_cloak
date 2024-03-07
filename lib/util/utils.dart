a7d8879d5185354b978592c0585f8e676212cc69
程梦义 <519097965@qq.com>
2024-03-07 19:11:13 +0800

    main

--- lib/util/utils.dart
+++ lib/util/utils.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 printLogByDebug(String string){
   if (kDebugMode) {
     print(string);
   }
 }
-
-// Fluttertoast.showToast(
-// msg: "kk==$kDebugMode",
-// toastLength: Toast.LENGTH_SHORT,
-// gravity: ToastGravity.CENTER,
-// timeInSecForIosWeb: 1,
-// backgroundColor: Colors.black45,
-// textColor: Colors.white);
